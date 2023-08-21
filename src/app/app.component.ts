28a81b13f9b7963339e76686fc048ea9419e77a2
codewithashith <codewithashith@gmail.com>
2023-08-21 17:27:19 +0530

    01. Bootstrap 5 setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-grocery-project';
+  title = 'Angular Grocery Project';
 }

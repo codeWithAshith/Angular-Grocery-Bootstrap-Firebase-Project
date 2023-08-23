47c535db1aa847b51962dd055498cf1728feab99
codewithashith <codewithashith@gmail.com>
2023-08-23 16:25:10 +0530

    02. Routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderComponent } from './components/order/order.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminOrdersComponent } from './components/admin/orders/orders.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { AdminProductsComponent } from './components/admin/products/products.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    CheckOutComponent,
+    OrderComponent,
+    LoginComponent,
+    AdminOrdersComponent,
+    NavbarComponent,
+    AdminOrdersComponent,
+    AdminProductsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'checkOut', component: CheckOutComponent },
+      { path: 'order', component: OrderComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/order', component: AdminOrdersComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/admin/orders/orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class AdminOrdersComponent {
+
+}
